427ed08343d088dbcc3ea4f87d276441f4a7a437
Dem Pilafian <dem@centerkey.com>
2018-12-08 02:17:23 -0800

    Upgrade to es6 notation and update dependencies

--- hljs-enhance.js
+++ hljs-enhance.js
@@ -1,18 +1,18 @@
 // hljs-enhance - https://github.com/center-key/hljs-enhance - MIT License
-var hljsEnhance = {
-   setup: function() {
-      function init(i, elem) {
+const hljsEnhance = {
+   setup: () => {
+      const init = (i, elem) => {
          elem = $(elem);
          var indent = '   ';
          var padding = new RegExp(elem.text().match(/\n[ \t]+/), 'g');
          elem.text($.trim(elem.text().replace(padding, '\n').replace(/\t/g, indent)));
          elem.parent().addClass('hljs-enhance').closest('figure').addClass('hljs-enhance');
          window.hljs.highlightBlock(elem[0]);
-         }
-      function onDocumentReady() {
+         };
+      const onDocumentReady = () => {
          $('pre code:not(.hljs-done)').each(init).addClass('hljs-done');
          $('figure.hljs-enhance >div').addClass('hljs');  //trim boxes without code
-         }
+         };
       $(onDocumentReady);
       }
    };
