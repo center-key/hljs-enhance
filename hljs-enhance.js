dd5210c754e8b8fb317b246d28a9d825bbc70c9b
dpilafian <dem@centerkey.com>
2022-10-03 12:39:27 -0700

    Convert to universal globalThis

--- hljs-enhance.js
+++ hljs-enhance.js
@@ -7,7 +7,7 @@ const hljsEnhance = {
          const padding = new RegExp(elem.text().match(/\n[ \t]+/), 'g');
          elem.text(elem.text().replace(padding, '\n').replace(/\t/g, indent).trim());
          elem.parent().addClass('hljs-enhance').closest('figure').addClass('hljs-enhance');
-         window.hljs.highlightElement(elem[0]);
+         globalThis.hljs.highlightElement(elem[0]);
          };
       const onDocumentReady = () => {
          $('pre code:not(.hljs-done)').toArray().forEach(init);
@@ -19,7 +19,6 @@ const hljsEnhance = {
 
 if (typeof module === 'object')
    module.exports = hljsEnhance;  //node module loading system (CommonJS)
-if (typeof window === 'object')
-   window.hljsEnhance = hljsEnhance;  //support both global and window property
+globalThis.hljsEnhance = hljsEnhance;
 
 hljsEnhance.setup();
